bc434a92e40bcf94770c8a77c769a045ab8961a5
Arnold Chen <arc013@ucsd.edu>
2017-02-15 17:19:29 -0800

    added basic yelp api

--- app.js
+++ app.js
@@ -15,20 +15,43 @@ var menu = require('./routes/menu');
 
 var app = express();
 
+
+//yelp api call
+//var Yelp = require('yelp');
+var Yelp = require('node-yelp-fusion');
+var yelp=new Yelp({ id:'jc6LBOrY9Y2ovxr0UIBbDw', secret:'sNsySmlXXSLoP2rg9jWwHe4lAKKa6qHnWYXSSWXqfIex6o1MRzTdcl7qlMDtuk6j'});
+/*var yelp = new Yelp({
+    consumer_key: 'consumer-key',
+    consumer_secret: 'consumer-secret',
+    token: 'token',
+    token_secret: 'token-secret',
+});*/
+
+
+
+
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('Intro HCI secret key'));
-app.use(express.session());
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(express.cookieParser('Intro HCI secret key'));
+//app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+yelp.reviews("red-moon-noodle-house-san-diego")
+    .then(function(result){
+        console.log(result);
+        res.json(result);
+    });
+
 
 // development only
 if ('development' == app.get('env')) {
