07303fe4b32aec8aee10c74f194394d64926d3df
Jeanette Phung <j5phung@ucsd.edu>
2017-03-15 17:52:03 -0700

    adjust menu

---
+++ public/js/about.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	
+	function matchHeight() {
+		insideH = $(".inside").height();
+		$(".victor").css("height", insideH);
+	}
+	
+	function displayImg() {
+		var winW = $(window).width();
+
+		if (winW > 767) {
+			$(".victor").css("display", "none");
+		} else {
+			$(".victor").css("display", "inherit");
+		}
+	}
+
+	matchHeight();
+	displayImg();
+	
+	$(window).resize(function() {
+		matchHeight();
+		displayImg();
+	});
+});
