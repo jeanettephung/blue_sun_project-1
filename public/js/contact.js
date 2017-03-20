691a782041e8cf1b327b73571396a028a5cb874f
Arnold Chen <arc013@ucsd.edu>
2017-03-19 17:41:27 -0700

    7

--- public/js/contact.js
+++ public/js/contact.js
@@ -11,3 +11,13 @@ $(document).ready(function() {
         console.log($("#contact_content").height());
     }
 });
+
+window.onresize = function() {
+    if (screen.width>992) {
+        var h = $("#contact_map").height();
+        console.log(h);
+        $("#front-image").css("height", h - 205);
+        console.log($("#contact_content").height());
+    }
+
+};
