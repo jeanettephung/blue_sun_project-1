95ecb3f490abd0fb3c111c2420a941d63b98ec66
Jeanette Phung <j5phung@ucsd.edu>
2017-03-25 18:32:33 -0700

    cleanup contact us and about

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,24 +1,25 @@
-/**
- * Created by arnoldchen on 3/19/17.
- */
 $(document).ready(function() {
-    console.log("google");
-
-    if (screen.width>992) {
-        var h = $("#contact_map").height();
-        console.log(h);
-        $("#front-image").css("height", h - 205);
-        console.log($("#contact_content").height());
-    }
+	if (screen.width>992) {
+		var h = $("#contact_map").height();
+		$("#front-image").css("height", h - 205);
+	}
 });
 
-window.onresize = function() {
-    if ($(window).width()>992) {
-        console.log("cool");
-        var h = $("#contact_map").height();
-        console.log(h);
-        $("#front-image").css("height", h - 205);
-        console.log($("#contact_content").height());
-    }
+function initMap() {
+	var uluru = {lat: 32.8250641, lng: -117.15585 };
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 19,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
 
+window.onresize = function() {
+	if ($(window).width()>992) {
+		var h = $("#contact_map").height();
+		$("#front-image").css("height", h - 205);
+	}
 };
