979e13d10b33e42dd5bb95aba95c9bc22c09002e
Jeanette Phung <j5phung@ucsd.edu>
2017-03-30 17:58:53 -0700

    process of converting css to less

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,7 @@ var less = require('gulp-less');
 var path = require('path');
 
 gulp.task('less', function () {
-  return gulp.src('./public/less/**/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
+  return gulp.src('./public/less/main.less')
+    .pipe(less())
     .pipe(gulp.dest('./public/css'));
 });
